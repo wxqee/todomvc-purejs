9024982c193740c3f9b9b016956dcf1b51037e89
xq21 <xq21@vip.qq.com>
2023-03-19 01:58:22 +0800

    init

--- js/app.js
+++ js/app.js
@@ -2,121 +2,169 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-	class TodoItemView {
+	class View {
+		_state = {};
+
+		get state() {
+			return this._state;
+		}
+
+		set state(value) {
+			Object.assign(this._state, value);
+			this._state = new Proxy(this._state, {
+				set: (o, p, v) => {
+					const result = Reflect.set(o, p, v);
+					this.render();
+					return result;
+				},
+			});
+		}
+	}
+
+	class TodoView extends View {
 		constructor({ todo }) {
+			super();
 			this.state = {
-				data: todo,
+				todo,
 				editing: false,
 			};
+			this.el = document.createElement('li');
+			this.registerEvents();
+			this.render();
+		}
+		registerEvents() {
+			this.el.addEventListener('dblclick', (e) => {
+				if (e.target === this.el.querySelector('label')) {
+					this.state.editing = true;
+					this.el.querySelector('input.edit').focus();
+					this.el.querySelector('input.edit').addEventListener('blur', (e) => {
+						e.preventDefault();
+						document.body.dispatchEvent(
+							new CustomEvent('todo.titlechanged', {
+								detail: {
+									id: this.state.todo.id,
+									title: this.el.querySelector('input.edit').value,
+								},
+							})
+						);
+						this.state.editing = false;
+					});
+				}
+			});
 
-			this.state = new Proxy(this.state, {
-				set: (o, p, v) => {
-					const result = Reflect.set(o, p, v);
-					this.render();
-					return result;
-				},
+			this.el.addEventListener('keypress', (e) => {
+				if (e.key === 'Enter') {
+					e.preventDefault();
+					this.state.editing = false;
+					// hidding (as blur) will save the new value
+				}
+			});
+
+			this.el.addEventListener('click', (e) => {
+				if (e.target === this.el.querySelector('button.destroy')) {
+					e.preventDefault();
+					document.body.dispatchEvent(
+						new CustomEvent('todo.delete', {
+							detail: {
+								id: this.state.todo.id,
+							},
+						})
+					);
+				}
 			});
 		}
-
-		renderHTML() {
-			const { data, editing } = this.state;
-			const { label, checked } = data;
-
-			return `
-			<li class="${checked ? 'completed' : ''} ${editing ? 'editing' : ''}">
-				<div class="view">
-					<input class="toggle" type="checkbox" ${checked ? 'checked' : ''} />
-					<label>${label}</label>
-					<button class="destroy"></button>
-				</div>
-				<input class="edit" value="${label}" />
-			</li>
-			`;
-		}
-
 		render() {
-			const template = document.createElement('template');
-			template.innerHTML = this.renderHTML();
-			return template.content.cloneNode(true);
+			const { todo, editing } = this.state;
+
+			this.el.innerHTML = `
+			<div class="view">
+				<input class="toggle" type="checkbox" ${todo.completed ? 'checked' : ''} />
+				<label>${todo.title}</label>
+				<button class="destroy"></button>
+			</div>
+			<input class="edit" value="${todo.title}" />
+			`;
+
+			this.el.classList.toggle('completed', todo.completed);
+			this.el.classList.toggle('editing', editing);
+
+			return this;
 		}
 	}
-	class TodoListView {
+
+	class TodoListView extends View {
 		constructor({ todos }) {
+			super();
 			this.state = {
 				todos,
 			};
-
-			this.state = new Proxy(this.state, {
-				set: (o, p, v) => {
-					const result = Reflect.set(o, p, v);
-					this.render();
-					return result;
-				},
-			});
+			this.el = document.createElement('ul');
+			this.render();
 		}
 
-		renderHTML() {
-			return `
+		render() {
+			this.el.innerHTML = `
 			<ul class="todo-list"></ul>
 			`;
-		}
 
-		render() {
-			const template = document.createElement('template');
-			template.innerHTML = this.renderHTML();
-			const $el = template.content.cloneNode(true);
+			this.el.classList.toggle('todo-list', true);
 
 			this.state.todos.forEach((todo) =>
-				$el
-					.querySelector('ul.todo-list')
-					.appendChild(new TodoItemView({ todo }).render())
+				this.el.appendChild(new TodoView({ todo }).el)
 			);
 
-			return $el;
+			return this;
 		}
 	}
 
-	class MainSectionView {
-		constructor({ el, todos }) {
-			this.el = el;
+	class MainSectionView extends View {
+		constructor({ todos }) {
+			super();
+			this.el = document.querySelector('.main'); // use existed dom
 			this.state = {
 				todos,
 			};
-
-			this.state = new Proxy(this.state, {
-				set: (o, p, v) => {
-					const result = Reflect.set(o, p, v);
-					this.render();
-					return result;
-				},
-			});
+			this.render();
 		}
 
 		renderHTML() {
-			return `
+			return;
+		}
+
+		render() {
+			const { todos } = this.state;
+
+			this.el.innerHTML = `
 			<input id="toggle-all" class="toggle-all" type="checkbox" />
 			<label for="toggle-all">Mark all as complete</label>
 			`;
-		}
 
-		render() {
-			const template = document.createElement('template');
-			template.innerHTML = this.renderHTML();
-			const $el = template.content.cloneNode(true);
+			this.el.replaceChildren(new TodoListView({ todos }).el);
 
-			const { todos } = this.state;
-			const $todos = new TodoListView({ todos }).render();
-			$el.appendChild($todos);
-
-			document.querySelector(this.el).replaceChildren($el);
+			return this;
 		}
 	}
 
 	const todos = [
-		{ id: 0, label: 1, checked: false },
-		{ id: 1, label: 2, checked: true },
-		{ id: 2, label: 3, checked: false },
+		{ id: 0, title: 1, completed: false },
+		{ id: 1, title: 2, completed: true },
+		{ id: 2, title: 3, completed: false },
 	];
 
-	new MainSectionView({ el: '.main', todos }).render();
+	const mainView = new MainSectionView({ todos });
+
+	document.body.addEventListener('todo.titlechanged', (e) => {
+		const { id, title } = e.detail;
+		todos.find((it) => id === it.id).title = title;
+		mainView.render();
+	});
+
+	document.body.addEventListener('todo.delete', (e) => {
+		const { id } = e.detail;
+		todos.splice(
+			todos.findIndex((it) => id === it.id),
+			1
+		);
+		mainView.render();
+	});
 })(window);
